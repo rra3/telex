539567cf06a0fd4353745cb2b1f2ac9f60d43ee1
RR Arnold <rra@warpmail.net>
2017-11-11 16:17:34 -0500

    wiring up express and pug

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 const port = 4000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+
+app.set('view engine', 'pug');
+app.set('views', './server/views');
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+  res.render('index', {title: 'telex for message passing',
+             message: 'hello world!'});
+});
+
+app.use('/', router);
 
 app.listen(port, () => console.log('listening on port %s', port));
